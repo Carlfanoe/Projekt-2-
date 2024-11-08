716df1d2a001a5f5fdc886c16b53b9e246559455
Carlfanoe <123903255+Carlfanoe@users.noreply.github.com>
2024-11-08 01:34:55 +0100

    Sletter nogle .h som ikke bruges endnu

--- Projekt 2/Brugergraenseflade.h
+++
@@ -1,6 +0,0 @@
-/*
- * Brugergraenseflade.cpp
- *
- * Created: 07/11/2024 22.40.27
- *  Author: carlf
- */ 
--- Projekt 2/Koer_automatisk_plantepleje.h
+++
@@ -1,6 +0,0 @@
-/*
- * Koer_automatisk_plantepleje.cpp
- *
- * Created: 07/11/2024 22.34.00
- *  Author: carlf
- */ 
